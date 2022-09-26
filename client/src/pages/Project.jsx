87d7a00596ea0e8f22468d210ec888bad7daedb7
yoshino9397 <yosino8512@gmail.com>
2022-09-25 17:18:30 -0700

    DONE

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
 import ClientInfo from "../components/ClientInfo";
 import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 
 const Project = () => {
   const { id } = useParams();
@@ -25,9 +26,7 @@ const Project = () => {
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
           <ClientInfo client={data.project.client} />
-          {/* 
-
-          <EditProjectForm project={data.project} /> */}
+          <EditProjectForm project={data.project} />
           <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
